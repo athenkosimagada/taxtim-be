414c5bc4b6df127453bb2cd912c079517b251af6
Athenkosi Magada <magadaathenkosi@gmail.com>
2026-02-01 19:13:27 +0200

    Implement CORS headers and handle preflight OPTIONS requests

--- index.php
+++ index.php
@@ -41,6 +41,19 @@ set_exception_handler(['ErrorHandler', 'handleException']);
 
 header('Content-Type: application/json; charset=UTF-8');
 
+// ==================
+// CORS
+// ==================
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
 /*
 |--------------------------------------------------------------------------
 | Dependencies
